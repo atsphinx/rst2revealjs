af1ff354393bcf2e90683b0a2b345ef11dde23fb
Kazuya Takei <myself@attakei.net>
2026-02-01 00:00:52 +0900

    fix: Split style from script

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,5 +1,4 @@
 import Alpine from "alpinejs";
-import "./style.css";
 import playground from "./contexts/playground";
 
 Alpine.data("playground", playground);
--- website/src/static.ts
+++
@@ -1,6 +0,0 @@
-import Alpine from "alpinejs";
-import "./style.css";
-import playground from "./contexts/playground";
-
-Alpine.data("playground", playground);
-Alpine.start();
